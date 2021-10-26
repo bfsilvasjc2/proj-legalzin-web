d6dbea3b2c80a7823ee62d522031719af4c2b7eb
Bruno <bfcarvalho2002@gmail.com>
2021-10-26 20:53:05 -0300

    initial commit

---
+++ PimWeb/PimWeb/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PimWeb.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string  SobreNome { get; set; }
+        public string  CPF { get; set; }
+        public int Idade { get; set; }
+        public string Telefone  { get; set; }
+
+
+    }
+}
